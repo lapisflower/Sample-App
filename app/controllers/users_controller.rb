efa30da89af211a88c5ee9a94a9dcd5ce9c481ce
lapisflower <lapisflower617@gmail.com>
2020-05-04 05:13:25 +0000

    D1 ユーザー編集更新ページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-   before_action :set_user
+   
  
   def show
     @user = User.find(params[:id])
@@ -24,11 +24,18 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
-  private
-    def set_user
-      @user = User.find(session[:user_id])
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "ユーザー情報を更新しました。"
+      redirect_to user_url
+    else render :edit
     end
-    
+  end
+  
+  
+  private
+   
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
